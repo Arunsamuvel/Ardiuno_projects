0754f05e120cd1afd79cd913f179ada90b3faba1
Arun S <154217512+Arunsamuvel@users.noreply.github.com>
2024-10-01 01:06:58 +0530

    Traffic light system .ino

--- sep10_healloworld_copy_20240930141207.ino
+++ sep10_healloworld_copy_20240930141207.ino
@@ -2,6 +2,7 @@
 void setup() {
   // put your setup code here, to run once:
  pinMode(7, OUTPUT);
+  //initialise PIN
  pinMode(4, OUTPUT);
  pinMode(9, OUTPUT);
 
@@ -9,20 +10,20 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(7, HIGH);
+digitalWrite(7, HIGH);// high means power supply 3-5v in DC
 delay(5000);
 digitalWrite(7, LOW);
-// 
+// ready section
 digitalWrite(4, HIGH);
 delay(1000);
 digitalWrite(4, LOW);
-// 
+// green section
 digitalWrite(9, HIGH);
 delay(5000);
 digitalWrite(9, LOW);
-
+// ready section 
 digitalWrite(4, HIGH);
 delay(1000);
-digitalWrite(4, LOW);
+digitalWrite(4, LOW);// low means powers goes to off or 0V
 
 }
