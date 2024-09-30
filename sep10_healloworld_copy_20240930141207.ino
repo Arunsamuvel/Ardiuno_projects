1958d250f76b06ebed6b54d1a80f11f9efd63b93
Arun S <154217512+Arunsamuvel@users.noreply.github.com>
2024-09-30 14:17:50 +0530

    Add files via upload

---
+++ sep10_healloworld_copy_20240930141207.ino
@@ -0,0 +1,28 @@
+//my first progarmming experiance 
+void setup() {
+  // put your setup code here, to run once:
+ pinMode(7, OUTPUT);
+ pinMode(4, OUTPUT);
+ pinMode(9, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(7, HIGH);
+delay(5000);
+digitalWrite(7, LOW);
+// 
+digitalWrite(4, HIGH);
+delay(1000);
+digitalWrite(4, LOW);
+// 
+digitalWrite(9, HIGH);
+delay(5000);
+digitalWrite(9, LOW);
+
+digitalWrite(4, HIGH);
+delay(1000);
+digitalWrite(4, LOW);
+
+}
